3fb403dd636aaf3b51ebedf8a47d8ef539cc2dbb
Ernesto <ernestoaguaysol@gmail.com>
2017-06-29 21:11:52 -0300

    grid, Fecha semana español

--- Calculadora/Calculadora/Models/Fecha.cs
+++ Calculadora/Calculadora/Models/Fecha.cs
@@ -4,8 +4,51 @@ namespace Calculadora.Models
 {
     public class Fecha
     {
-
+        #region Properties
         public DateTime DateTime { get; set; }
 
+        public string Dia
+        {
+            get
+            {
+                var dia = DiaSemana(DateTime.DayOfWeek.ToString());
+                return dia;
+            }
+        }
+        #endregion
+
+        #region Methods
+        private string DiaSemana(string diaSemana)
+        {
+            string dia = string.Empty;
+            switch (diaSemana)
+            {
+                case "Sunday":
+                    dia = "Domingo";
+                    break;
+                case "Monday":
+                    dia = "Lunes";
+                    break;
+                case "Tuesday":
+                    dia = "Martes";
+                    break;
+                case "Wednesday":
+                    dia = "Miersoles";
+                    break;
+                case "Thursday":
+                    dia = "Jueves";
+                    break;
+                case "Friday":
+                    dia = "Viernes";
+                    break;
+                case "Saturday":
+                    dia = "Sabado";
+                    break;
+                default:
+                    break;
+            }
+            return dia;
+        }
+        #endregion
     }
-}
+    }
